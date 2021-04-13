731e0e133264a8824c39f63c4989d8f855bb4472
Carlos Klein <c.edudepaula@yahoo.com.br>
2021-04-13 20:05:58 -0300

    Aplicação princípios SOLID

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post("/", (request, response) => {
+  return createSpecificationController.handle(request, response);
+});
+
+// specificationsRoutes.get("/", (request, response) => {
+//   const specifications = specificationsRepository.list();
+//   return response.status(200).json(specifications);
+// });
+
+export { specificationsRoutes };
